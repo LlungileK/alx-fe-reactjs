2c97f26b98682d9fa0dca4d2d60105d34704f165
LlungileK <l.khoza@lab.clicklearning.org>
2024-11-07 20:17:05 +0200

    Added alx-fe-app-new

---
+++ alx-fe-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'lightgray', color: 'black', textAlign: 'center', padding: '10px' }}>
+      <p>© 2023 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
