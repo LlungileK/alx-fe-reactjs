a09417933f204cc16a2c5a9f4d7d570004c56f1e
LlungileK <l.khoza@lab.clicklearning.org>
2024-11-08 19:47:04 +0200

    Delete alx-react-app-props directory

--- alx-react-app-props/src/components/UserContext.js
+++
@@ -1,7 +0,0 @@
-import React, { createContext } from 'react';
-
-// Initialize UserContext
-const UserContext = createContext();
-
-// Export the context to use in other components
-export default UserContext;
