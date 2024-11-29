a84a48de0d8fb1293df20e72ee24ea08d34da59e
LlungileK <l.khoza@lab.clicklearning.org>
2024-11-29 19:35:22 +0200

    Updated UserProfile.jsx and App.jsx in directory

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import UserProfile from './components/UserProfile'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
+
